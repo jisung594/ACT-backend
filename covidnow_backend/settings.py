1d2577dd52baf157fee494938e9e7e2d6a86a706
Jonathan Choi <jisung594@gmail.com>
2020-07-07 16:50:46 -0400

    included missing files

--- covidnow_backend/settings.py
+++ covidnow_backend/settings.py
@@ -24,12 +24,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = ''
+# ***** key hidden in secret_settings.py file *****
+# ***** secret_setting.py file included in .gitignore *****
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['']
-# ALLOWED_HOSTS = ['http://127.0.0.1/']  #---------------
+# ALLOWED_HOSTS = ['http://127.0.0.1/']
 
 
 # Application definition
